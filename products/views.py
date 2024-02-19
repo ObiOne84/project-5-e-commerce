d2396d6283e2d3680085ab0f4ce4ea88f4b6c67c
ObiOne84 <stepienszymon@hotmail.com>
2024-02-19 20:09:55 +0000

    Add paginator to producst page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
 from .models import Comic, Book
 
 def all_products(request):
@@ -6,12 +8,20 @@ def all_products(request):
 
     comics = Comic.objects.all()
     books = Book.objects.all()
-
     products = list(comics) + list(books)
+    total_products = len(products)
+    is_paginated = len(products) > 18
+    paginator = Paginator(products, 18)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'products': products,
+        'products': page_obj,
+        'page_obj': page_obj,
+        'paginator': paginator,
+        'is_paginated': is_paginated,
+        'total_products': total_products,
     }
 
     return render(request, 'products/products.html', context)
-
