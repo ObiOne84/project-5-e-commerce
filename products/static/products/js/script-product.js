90bdae0e29a9cd7debfd2000aeb52424f3603e9d
ObiOne84 <stepienszymon@hotmail.com>
2024-02-21 14:52:39 +0000

    Update product detail page - add buttons and tabs

--- products/static/products/js/script-product.js
+++ products/static/products/js/script-product.js
@@ -6,15 +6,9 @@ $(document).ready(function () {
         $(this).addClass('active');
 
         var index = $(this).parent().index();
-        var boxLength = $('.extra-content').length;
-        var linkLength = $(this).length;
 
         $('.extra-content').removeClass('d-md-block').addClass('d-md-none');
         $('.extra-content').eq(index).removeClass('d-md-none').addClass('d-md-block');
-        console.log(index);
-        console.log('-----------------------')
-        console.log(boxLength);
-        console.log(linkLength);
     });
 
     // Display truncated text
@@ -28,11 +22,10 @@ $(document).ready(function () {
 
     // Add and remove active class for mobile device when toggle
     $('.collapse-link').on('click', function () {
-        // e.preventDefault();
         if ($(this).hasClass('active')) {
-            $(this).removeClass('active'); // Remove the active class if it exists
+            $(this).removeClass('active');
         } else {
-            $(this).addClass('active'); // Add the active class if it doesn't exist
+            $(this).addClass('active');
         }
     });
 
