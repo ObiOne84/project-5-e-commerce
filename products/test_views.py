97e9b3a37e2999a8f7d94a0a0ca6b5951d940097
ObiOne84 <stepienszymon@hotmail.com>
2024-02-21 21:21:07 +0000

    Add test views file for products app

---
+++ products/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import Book, Comic, Category, Subcategory
+
+
+class ProductDetailViewTest(TestCase):
+    def setUP(self):
+        self.category = Category.objects.create(name='Fiction')
+        self.subcategory = Subcategory.objects.create(name='Sci-Fi')
+        self.book = Book.objects.create(
+            title='Test Book',
+            author='Book Author',
+            description="Great book",
+            category= self.category,
+            subcategory=self.subcategory,
+        )
+        self.book = Comic.objects.create(
+            title='Comic Book',
+            author='Comic Author',
+            description="Great comic",
+            category= self.category,
+            subcategory=self.subcategory,
+        )
+
+    def text_existing_product_detail(self):
+        # Test if the view renders correctly for exisiting products
+        response = self.client.get(reverse('product_detail', args=[self.book.pk]))
+        self.assertEqual(response.status_code, 200)
