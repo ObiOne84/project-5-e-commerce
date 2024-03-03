368bb6388b3726559bae6ee010e6ebb7b9adabd1
ObiOne84 <stepienszymon@hotmail.com>
2024-03-03 20:09:10 +0000

    Add script for incrementing and decrementing product quantity

---
+++ products/static/products/js/quantity_input_script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+// Source: Boutique Ado walkthrough project
+
+     // Disable +/- buttons outside 1-99 range
+     function handleEnableDisable(itemId) {
+        var currentValue = parseInt($(`#id_qty_${itemId}`).val());
+        var minusDisabled = currentValue < 2;
+        var plusDisabled = currentValue > 9;
+        $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+        $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
+
+    }
+
+    // Ensure proper enabling/disabling of all inputs on page load
+    var allQtyInput = $('.qty_input');
+    for (var i = 0; i < allQtyInput.length; i++) {
+        var itemId = $(allQtyInput[i]).data('item_id');
+        handleEnableDisable(itemId);
+    }
+
+    // Check enable/disable every time the input is changed
+    $('.qty_input').change(function() {
+        var itemId = $(this).data('item_id');
+        handleEnableDisable(itemId);
+    });
+
+    // Increment quantity
+    $('.increment-qty').click(function(e) {
+        e.preventDefault();
+        var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+        var currentValue = parseInt($(closestInput).val());
+        $(closestInput).val(currentValue + 1)
+        handleEnableDisable(itemId);
+    });
+
+    // Decrement quantity
+    $('.decrement-qty').click(function(e) {
+        e.preventDefault();
+        var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+        var currentValue = parseInt($(closestInput).val());
+        $(closestInput).val(currentValue - 1)
+        handleEnableDisable(itemId);
+    });
+});
