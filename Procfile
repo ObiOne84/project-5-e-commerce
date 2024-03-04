adc8f1a9d6884ff0e6fe3e5d2b99a36215bb7d32
ObiOne84 <stepienszymon@hotmail.com>
2024-03-04 20:25:13 +0000

    Update settings for database

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn owl_bookstore.wsgi:application
--- owl_bookstore/settings.py
+++ owl_bookstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('LOCAL_HOST'),]
+ALLOWED_HOSTS = [os.environ.get('LOCAL_HOST'), 'owl-bookstore-2a747a64a0a9.herokuapp.com/',]
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'owl_bookstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# if 'DATABASE_URL' in os.environ:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
