ab960cc870aeb903fcd30e6600c30caf9f089ace
ObiOne84 <stepienszymon@hotmail.com>
2024-03-05 10:14:14 +0000

    Update settings.py

--- owl_bookstore/settings.py
+++ owl_bookstore/settings.py
@@ -177,8 +177,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS settings
 if 'USE_AWS' in os.environ:
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.eu-west-1.s3.amaznoaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     # 'https://owl-bookstore.s3.eu-west-1.amazonaws.com/static/'
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
