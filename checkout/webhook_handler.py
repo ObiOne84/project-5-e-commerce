41d9aa8cc3099b3c735c9af94745f932aeb2d4b7
ObiOne84 <stepienszymon@hotmail.com>
2024-03-10 16:54:09 +0000

    Add class method to webhood handler and webhook view

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 
 
+# Source: Boutique Ado walkthrough project
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
@@ -11,6 +12,22 @@ class StripeWH_Handler:
         """Handle a generic/unknow/unexpected webhook event"""
 
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """Handle the payment_intent.succeeded webhook from Stripe"""
+
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """Handle the payment_intent.payment_failed webhook from Stripe"""
+
+        return HttpResponse(
+            content=f'Payment Failed Webhook received: {event["type"]}',
             status=200
         )
