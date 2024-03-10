8f3dbcd62c724e9ba59af0774304b847305d6580
ObiOne84 <stepienszymon@hotmail.com>
2024-03-10 13:41:20 +0000

    Update checkout success page for order of 10 or more items

--- bag/context.py
+++ bag/context.py
@@ -31,6 +31,7 @@ def bag_content(request):
             'product': product,
         })
 
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PRECENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
