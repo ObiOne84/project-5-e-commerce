3b498e2e4867f7ba179cc498de8a85fcfaf03e5e
ObiOne84 <stepienszymon@hotmail.com>
2024-03-11 21:06:59 +0000

    Add profiles to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    model = UserProfile
+
+    exlude = ('user',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
