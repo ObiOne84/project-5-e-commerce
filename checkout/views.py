143473da2b0bee23912af31463260f85176ef054
ObiOne84 <stepienszymon@hotmail.com>
2024-03-13 22:03:34 +0000

    Add confirmation email after order

--- checkout/views.py
+++ checkout/views.py
@@ -22,7 +22,7 @@ def cache_checkout_data(request):
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
-            # 'username': request.user,
+            'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
