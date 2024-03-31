46a27d184c975345d86f181a34d94746cc48d123
ObiOne84 <stepienszymon@hotmail.com>
2024-03-31 21:13:46 +0000

    Update logic for error 500

--- owl_bookstore/views.py
+++ owl_bookstore/views.py
@@ -5,6 +5,7 @@ def handler404(request, exception):
     """Error Handler 404 - Page Not Found"""
     return render(request, "errors/404.html", status=404)
 
-def handler500(request, exception):
+
+def handler500(request):
     """Error Handler 500 - Internal Server Error"""
     return render(request, "errors/500.html", status=500)
