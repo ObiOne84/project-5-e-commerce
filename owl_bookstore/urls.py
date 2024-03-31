2e3fd693c8b56c6a28e81d46ea095481e28631e8
ObiOne84 <stepienszymon@hotmail.com>
2024-03-31 20:53:09 +0000

    Add error404 page

--- owl_bookstore/urls.py
+++ owl_bookstore/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 urlpatterns = [
@@ -28,3 +29,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'owl_bookstore.views.handler404'
