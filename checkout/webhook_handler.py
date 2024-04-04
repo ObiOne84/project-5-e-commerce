56a1624f39ecc9d12e7394f4348e3276b6d72546
ObiOne84 <stepienszymon@hotmail.com>
2024-04-04 14:02:21 +0000

    Update checkout view.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -49,15 +49,18 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
+        print(intent, '#1')
         pid = intent.id
+        print(pid, '#2')
         bag = intent.metadata.bag
+        print(bag, '#3')
         save_info = intent.metadata.save_info
 
         # Get the Charge object
         stripe_charge = stripe.Charge.retrieve(
             intent.latest_charge
         )
-
+        print(stripe_charge, '#4')
         billing_details = stripe_charge.billing_details  # updated
         shipping_details = intent.shipping
         grand_total = round(stripe_charge.amount / 100, 2)  # updated
