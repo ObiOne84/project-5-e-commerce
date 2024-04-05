c942302683178bc98c663edf66b3785dfe76f959
ObiOne84 <stepienszymon@hotmail.com>
2024-04-05 13:22:47 +0000

    Update user profile - full name added

--- profiles/forms.py
+++ profiles/forms.py
@@ -14,7 +14,7 @@ class ProfileForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            # 'default_full_name': 'Full Name',
+            'default_full_name': 'Full Name',
             'default_phone_number': 'Phone Number',
             'default_postcode': 'Postal Code',
             'default_town_or_city': 'Town or City',
@@ -23,7 +23,7 @@ class ProfileForm(forms.ModelForm):
             'default_county': 'County',
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['default_full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
                 if self.fields[field].required:
